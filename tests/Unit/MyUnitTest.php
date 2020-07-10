4a62b479fbf1cdfc6ca6e5085f7d556b6fad51cb
Kristian Nissen <kristian.nissen@gmail.com>
2020-07-10 10:38:05 +0200

    Mockery setup

--- tests/Unit/MyUnitTest.php
+++ tests/Unit/MyUnitTest.php
@@ -1,5 +1,15 @@
 <?php
 
+use \Mockery as m;
+
+beforeEach(function() {
+    
+});
+
+afterEach(function() {
+    m::close();
+});
+
 test('assert true is true', function() {
     assertTrue(true);
 });
