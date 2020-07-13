d5e922f860b959a0dba4200a9e84a9d1ba65c07d
Kristian Nissen <kristian.nissen@gmail.com>
2020-07-13 17:51:26 +0200

    Removing old Ping class

--- src/Account/Account.php
+++ src/Account/Account.php
@@ -5,32 +5,8 @@
  */
 namespace QuickPay\Account;
 
-use GuzzleHttp\Client;
+use QuickPay\QuickPayModel;
 
-class Account
+class Pong extends QuickPayModel
 {
-    protected $attributes = [];
-
-    public function __construct(array $attributes = [])
-    {
-    }
-
-    /**
-     * Performs GET request
-     *
-     * @return object Account
-     */
-    public static function get(): Account
-    {
-        $client = new Client([
-            'base_uri' => 'https://api.quickpay.net',
-        ]);
-        $response = $client->request('GET', 'account', [
-            'headers' => [
-                'Accept-Version' => 'v10',
-                'Accept' => 'application/json',
-            ],
-            'auth' => [],
-        ]);
-    }
 }
